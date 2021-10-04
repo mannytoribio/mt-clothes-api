1f12e1ee0e7683e9a51b9fb1a290f6834fa0eb1b
Manny Toribio <manny.mtoribio@gmail.com>
2021-10-04 16:53:15 -0400

    full CRUD firebase API

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
-const { getAllProducts } = require('./src/products')
+const { getAllProducts, getPrductById, createProduct, updateProduct, deleteProduct } = require('./src/products')
 
 const app = express()
-
+app.use(express.json())
 
 
 // take a request and response for a single endpoint
 // .get is for get requests, takes the end point as the first parameter and the request/response package as a second parameter
 // app.get('/products', (req, res) => getAllProducts(req, res)); short hand version below:
+app.get('/products/:id', getPrductById)
+app.patch('/products/:id', updateProduct)
+app.delete('/products/:id', deleteProduct)
 app.get('/products', getAllProducts)
-
-
+app.post('/products', createProduct)
 
 //opens port and listen to requests coming through this port. The console log confirms that the program is listening
 app.listen(3000, () => {
--- src/products.js
+++ src/products.js
@@ -1,11 +1,13 @@
 // connect to database
 const { connectDB } = require('./db')
 
+const CLOTHES_COLLECTION = 'clothes'
+const db = connectDB()
+
 // get all products
 // takes req(uest) & res(ponse)
 exports.getAllProducts = (req, res) => {
-  const db = connectDB()
-  db.collection('clothes').get()
+  db.collection(CLOTHES_COLLECTION).get()
     .then(collection => {
       const products = collection.docs.map(doc => {
         let product = doc.data()
@@ -19,8 +21,47 @@ exports.getAllProducts = (req, res) => {
 
 // get a single prodduct
 
+exports.getPrductById = (req, res) => {
+  const { id } = req.params
+
+  db.collection(CLOTHES_COLLECTION).doc(id).get()
+  .then(doc => {
+    let product = doc.data()
+    product.id = doc.id
+    res.send(product)
+  })
+  .catch(error => res.status(500).send(error))
+}
+
 // create a new product
+
+exports.createProduct = (req, res) => {
+  const newProduct = req.body
+  db.collection(CLOTHES_COLLECTION).add(newProduct)
+  .then(docRef => {
+    let product = req.body
+    product.id = docRef.id
+    res.send(product)
+  })
+}
 
 // update a single product
 
+exports.updateProduct = (req, res) => {
+  const product = req.body
+  const { id } = req.params
+  db.collection(CLOTHES_COLLECTION).doc(id).update(product)
+  .then(() => this.getPrductById(req,res))
+  .catch(error => res.status(500).send(error))
+}
+
 // delete a single product
+
+exports.deleteProduct = (req, res) => {
+  const { id } = req.params
+  db.collection(CLOTHES_COLLECTION).doc(id).delete()
+  .then(timeStamp => {
+  res.status(200).send(`Product ${id} nuked at ${timeStamp}`) 
+  })
+  .catch(error => res.status(500).send(error))
+}
